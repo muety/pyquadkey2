54efaa2f55a27ce081bbe7b271538e5a51d47e1c
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2019-11-02 22:28:42 +0100

    Add setup script for PyPi distribution.

--- README.md
+++ README.md
@@ -22,10 +22,19 @@ For more details on the concept, please refer to the [original article](https://
 This library requires **Python 3.6** or higher. To compile it by yourself, Cython is required in addition.
 
 ### Using Pip
-`pip3 install pyquadkey2`
+* `pip3 install pyquadkey2`
+
+Currently, there are pre-built `linux_x86_64` for Python 3.6, 3.7 and 3.8. If you're on a Linux system, you can simply use pip without any additional requirements.
+On other systems, you might need to install Cython first (`pip3 install cython`). If you encounter problems while installing, please report them as a new issue.
 
 ### From Source
-TODO
+* `git clone https://github.com/n1try/pyquadkey2`
+* `pip3 install cython`
+* `cd pyquadkey2/quadkey/tilesystem && python3 setup.py build_ext --inplace && ../../`
+* `pip3 install .`
+
+### Troubleshooting
+* `ImportError: cannot import name 'tilesystem'`: Simply try `pip3 install --upgrade pyquadkey2` once again. Second time usually works, as required build extensions are installed then. This is a known issue and will be fixed in the future.
 
 ## License
 Apache 2.0
--- setup.py
+++ setup.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python
+# https://packaging.python.org/tutorials/packaging-projects/
+import os
 
 from setuptools import setup, find_packages
+from setuptools.glob import glob
+
+try:
+    from Cython.Distutils.extension import Extension
+    from Cython.Build import build_ext
+except ImportError:
+    from setuptools import Extension
+
+    USING_CYTHON = False
+else:
+    USING_CYTHON = True
+
+ext = 'pyx' if USING_CYTHON else 'c'
+sources = glob('quadkey/tilesystem/*.%s' % (ext,))
+extensions = [Extension(source.split('.')[0].replace(os.path.sep, '.'), sources=[source]) for source in sources]
+cmdclass = {'build_ext': build_ext} if USING_CYTHON else {}
 
 setup(
-      name='pyquadkey2',
-      version='0.1.0',
-      description='Python implementation of geographical tiling using QuadKeys as proposed by Microsoft',
-      author='Ferdinand Mütsch',
-      author_email='ferdinand@muetsch.io',
-      url='https://github.com/n1try/pyquadkey2',
-      packages=find_packages(),
-      install_requires=['Cython>=0.29.12'],
-      project_urls={
-            'Bug Tracker': 'https://github.com/n1try/pyquadkey2/issues',
-            'Source Code': 'https://github.com/n1try/pyquadkey2',
-      },
-      keywords='tiling quadkey quadtile geospatial geohash'
+    name='pyquadkey2',
+    version='0.1.0',
+    description='Python implementation of geographical tiling using QuadKeys as proposed by Microsoft',
+    author='Ferdinand Mütsch',
+    author_email='ferdinand@muetsch.io',
+    url='https://github.com/n1try/pyquadkey2',
+    packages=find_packages(),
+    project_urls={
+        'Bug Tracker': 'https://github.com/n1try/pyquadkey2/issues',
+        'Source Code': 'https://github.com/n1try/pyquadkey2',
+    },
+    keywords='tiling quadkey quadtile geospatial geohash',
+    python_requires='>=3.6',
+    ext_modules=extensions,
+    cmdclass=cmdclass
 )
