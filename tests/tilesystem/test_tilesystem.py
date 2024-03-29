6d7ac66330bd208de31be9a74b487353e2cc06ae
Brian Piper <bpiper@apple.com>
2024-03-29 16:25:28 -0700

    Update Cython and test python 3.7

--- tests/tilesystem/test_tilesystem.py
+++ tests/tilesystem/test_tilesystem.py
@@ -1,8 +1,9 @@
 import sys
+import os
 import unittest
 from unittest import TestCase
 
-sys.path.append('../src')
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../src'))
 
 from pyquadkey2.quadkey import TileAnchor
 from pyquadkey2.quadkey.tilesystem import tilesystem
