7b1d5370f2dd603e89c21b56e979b55b4cb9e082
Brian Piper <bpiper@apple.com>
2024-04-02 14:09:50 -0700

    Bump pyquadkey2 version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='pyquadkey2',
-    version='0.2.2',
+    version='0.3.2',
     description='Python implementation of geographical tiling using QuadKeys as proposed by Microsoft',
     long_description=long_description,
     long_description_content_type='text/markdown',
